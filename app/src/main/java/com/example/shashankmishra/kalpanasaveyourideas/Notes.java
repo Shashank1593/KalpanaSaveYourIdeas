7fc42e8ab7b51d7460f0adbc57616c59072d858c
Shashank Mishra <redcat1593@gmail.com>
2018-10-28 00:40:26 +0530

    Kalpana

--- app/src/main/java/com/example/shashankmishra/kalpanasaveyourideas/Notes.java
+++ app/src/main/java/com/example/shashankmishra/kalpanasaveyourideas/Notes.java
@@ -1,7 +1,11 @@
 package com.example.shashankmishra.kalpanasaveyourideas;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Notes extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class Notes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+    }
+
+    public void noteBack(View view)
+    {
+
+        Intent intent=new Intent(Notes.this,TheWall.class);
+        startActivity(intent);
+    }
+
+    //Alert Box
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Exit ?")
+                .setMessage("Are you Sure Then Press YES")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Notes.super.onBackPressed();
+                    }
+                })
+                .setCancelable(true)
+                .show();
+
+
     }
 }
